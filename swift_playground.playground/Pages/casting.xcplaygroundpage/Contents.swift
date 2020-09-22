ebd4619bda9d0be0b4bfaa49825009f6780d74ff
Thien Tran <thien.tran@dfo.global>
2020-09-22 22:02:46 +0700

    Anything about casting data type

--- swift_playground.playground/Pages/casting.xcplaygroundpage/Contents.swift
+++ swift_playground.playground/Pages/casting.xcplaygroundpage/Contents.swift
@@ -29,7 +29,53 @@ class Fish : Animal {
 }
 
 let dog = Dog(name : "PitBull")
-let human = Human(name : "Crazy Man")
-let fish = Fish(name: "Gold Fish")
-let animals = [dog, human,fish]
+let people = Human(name : "Crazy Man")
+let goldFish = Fish(name: "Gold Fish")
 
+if goldFish is Fish {
+    print("This is fish")
+}
+
+if goldFish is Animal {
+    print("Fish is also animal")
+}
+
+if people is Fish {
+    print("People is fish")
+}else{
+    print("People is not fish")
+}
+
+print("---------------forced downcast -----------------")
+
+//Mark : Cast object from base class type to subclass type
+
+let animals = [dog, people, goldFish]
+for animal in animals {
+    if animal is Fish {
+        //animal.swim() wrong
+        // has to cast to correct
+        
+        let fish = animal as! Fish
+        fish.swim()
+    }
+}
+
+print("-------------------- casting-------------------")
+//let smallDog = animals[0] as? Dog
+//smallDog?.run()
+//or
+if let smallDog = animals[0] as? Dog {
+    smallDog.run()
+}else{
+    print("Casting has failed")
+}
+
+
+var woman : Human?
+//woman = nil
+woman = Human(name: "Truc")
+woman?.walk()
+ 
+
+print("--------------------Any/AnyObject/NSObject-----------------------------")
